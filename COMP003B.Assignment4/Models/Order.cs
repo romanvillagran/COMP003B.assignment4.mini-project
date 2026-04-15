59145851f8555a44c7e0efca29a5666abd951ae6
cbig3 <romanvillagran386@gmail.com>
2026-04-15 15:54:34 -0700

    added order model with a required product name, the quantity

---
+++ COMP003B.Assignment4/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.Assignment4.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+        [Range(1, 100)]
+        public int Quantity { get; set; }
+
+      public Customer Customer { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
