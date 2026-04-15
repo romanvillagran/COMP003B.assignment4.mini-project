d10409fc5e854eedf27df4218a98055a7ae643a5
cbig3 <romanvillagran386@gmail.com>
2026-04-15 15:17:53 -0700

    added a customer model with required name, email, and phone

---
+++ COMP003B.Assignment4/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace COMP003B.Assignment4.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email {  get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Phone {  get; set; }
+    }
+}
