b34054f5bbfb92dd795ee67123065b21ebdbee23
cbig3 <romanvillagran386@gmail.com>
2026-04-15 16:14:50 -0700

    added the first controller with a success view and a create order

---
+++ COMP003B.Assignment4/Controllers/OrdersController.cs
@@ -0,0 +1,29 @@
+﻿using COMP003B.Assignment4.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace COMP003B.Assignment4.Controllers
+{
+    public class OrdersController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Success");
+            }
+            return View(order);
+        }
+
+        [HttpGet]
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
