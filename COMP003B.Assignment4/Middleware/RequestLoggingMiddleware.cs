78c2756be0a0b32008612d77ca5ec11c7df2268d
cbig3 <romanvillagran386@gmail.com>
2026-04-16 10:13:37 -0700

    added the middleware file and added custom middleware to it

--- COMP003B.Assignment4/Middleware/RequestLoggingMiddleware.cs
+++ COMP003B.Assignment4/Middleware/RequestLoggingMiddleware.cs
@@ -2,6 +2,17 @@
 {
     public class RequestLoggingMiddleware
     {
-        
+        private readonly RequestDelegate _next;
+
+        public RequestLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine($"Request: {context.Request.Path}");
+            await _next(context);
+        }
     }
 }
