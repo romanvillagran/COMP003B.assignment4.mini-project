4c43e306aebd047370ed57f98284bed30ac1c28f
cbig3 <romanvillagran386@gmail.com>
2026-04-15 22:52:54 -0700

    added the register cshtml file as well as a way to register aan event

---
+++ COMP003B.Assignment4/Views/Events/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP003B.Assignment4.Views.Events
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
