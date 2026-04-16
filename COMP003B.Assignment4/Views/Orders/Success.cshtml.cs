e2d013a2841b559a236dc0959169e626b3d51c86
cbig3 <romanvillagran386@gmail.com>
2026-04-15 22:33:32 -0700

    aded the success cshtml file as well as a confirmation

---
+++ COMP003B.Assignment4/Views/Orders/Success.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP003B.Assignment4.Views.Orders
+{
+    public class SuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
