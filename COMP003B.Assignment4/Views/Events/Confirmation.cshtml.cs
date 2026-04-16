06f112a5cd37b9768b7ca724e2dee32a5acfe6de
cbig3 <romanvillagran386@gmail.com>
2026-04-15 22:58:05 -0700

    added the confirmation cshtml file with a comfirmation message

---
+++ COMP003B.Assignment4/Views/Events/Confirmation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP003B.Assignment4.Views.Events
+{
+    public class ConfirmationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ COMP003B.Assignment4/Views/Shared/_Menu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP003B.Assignment4.Views.Shared
+{
+    public class _MenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
