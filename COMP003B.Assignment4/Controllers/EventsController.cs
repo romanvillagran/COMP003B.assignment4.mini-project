38d55b8fc00a051c505cb6a4669b4eae67dc1790
cbig3 <romanvillagran386@gmail.com>
2026-04-15 21:09:41 -0700

    added an event controller with the code. my brain is fried and i dont want to ecplain it

---
+++ COMP003B.Assignment4/Controllers/EventsController.cs
@@ -0,0 +1,31 @@
+﻿using COMP003B.Assignment4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment4.Controllers
+{
+    [Route("events")]
+    public class EventsController : Controller
+    {
+        [HttpGet("register/{id:int}")]
+        public IActionResult Register(int id)
+        {
+            return View();
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Confirmation");
+            }
+            return View(order);
+        }
+        [HttpGet("confirmation")]
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+        
+    }
+}
